7e52845159261747546f373fbc8d080243780f11
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-24 19:45:18 +0300

    auto: 2026-04-24 19:45

--- src/components/blog/projectText.tsx
+++ src/components/blog/projectText.tsx
@@ -5,7 +5,7 @@ import { nunitoSans } from "@/app/fonts";
 function ProjectText({title,desc, className ,data_aos}:{title:string,desc:string,className?:string,data_aos?:string}) {
   return (
           <div data-aos={data_aos} className={className} >
-                <p className={`${nunito.className} text-black text-xl text-center`}>{title}</p>
+                <p className={`${nunitoSans.className} font-light text-black text-xl text-center`}>{title}</p>
                 
               </div>
   )
