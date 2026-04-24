7e2c73dddfd10a4ecb25eb4f371a17d7a8a94d93
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-24 19:40:17 +0300

    auto: 2026-04-24 19:40

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -39,16 +39,6 @@ const nunito = Nunito_Sans({
   display: "swap",
 });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 
 
