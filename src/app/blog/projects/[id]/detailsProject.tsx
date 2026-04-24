51c0930b179bd8cb1b37bf6c443850bbc73c5f48
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-24 17:17:20 +0300

    auto: 2026-04-24 17:17

--- src/app/blog/projects/[id]/detailsProject.tsx
+++ src/app/blog/projects/[id]/detailsProject.tsx
@@ -75,7 +75,7 @@ function  DetailsProject({id}:{id:string}) {
         close={() => setIndex(-1)}
         slides={project?.ProjectImages?.map((el,i)=>{
           const slideImg:SlideImage={
-            src:`${process.env.NEXT_PUBLIC_HOST+"/dashboard"+el.url}`
+            src:`${process.env.NEXT_PUBLIC_STRAPI_BASE+el.url}`
           }
           return slideImg
         } )
