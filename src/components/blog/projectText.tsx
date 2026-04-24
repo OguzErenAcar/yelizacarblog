a428bde9c81655941e8d2d02111a82b68cd098f6
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-24 19:45:12 +0300

    auto: 2026-04-24 19:45

--- src/components/blog/projectText.tsx
+++ src/components/blog/projectText.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import { Nunito_Sans } from 'next/font/google';
-
-const nunito = Nunito_Sans({
-  subsets: ['latin'],
-  weight: ['300'],       // sadece regular
-  display: 'swap',
-});
+import { nunitoSans } from "@/app/fonts";
 
 
 function ProjectText({title,desc, className ,data_aos}:{title:string,desc:string,className?:string,data_aos?:string}) {
